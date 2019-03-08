3ca377dc6d1070db9cf7f31014fc913cdd9269e9
sexyo <elesex14@gmail.com>
2019-03-08 17:29:16 +0900

    [ 2019-03-08 ]
    김현석
    
    A, B , C 변경

--- vision/src/com/display/B.java
+++ vision/src/com/display/B.java
@@ -3,7 +3,7 @@ package com.display;
 public class B {
 
 	public static void main(String[] args) {
-		String B = "피카츄";
+		String B = "최형원";
 		System.out.println(B);
 
 
--- vision/src/com/display/C.java
+++ vision/src/com/display/C.java
@@ -3,7 +3,7 @@ package com.display;
 public class C {
 
 	public static void main(String[] args) {
-		String C = "파이리";
+		String C = "주승만";
 		System.out.println(C);
 
 
