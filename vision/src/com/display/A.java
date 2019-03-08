f4018227258a4fb2db0c886024c5b1a8bf3d9496
heeheedang <heesang0715@gmail.com>
2019-03-08 16:53:01 +0900

    [ 2019-03-08 ]
    황희상
    
    프로젝트추가

---
+++ vision/src/com/display/A.java
@@ -0,0 +1,11 @@
+package com.display;
+
+public class A {
+
+	public static void main(String[] args) {
+	String A = "김현석";
+	System.out.println(A);
+
+	}
+
+}
---
+++ vision/src/com/display/B.java
@@ -0,0 +1,12 @@
+package com.display;
+
+public class B {
+
+	public static void main(String[] args) {
+		String B = "황희상";
+		System.out.println(B);
+
+
+	}
+
+}
