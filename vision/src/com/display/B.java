62777cbbe7bd0b2dfeca9119db637ac6e565c4d3
heeheedang <heesang0715@gmail.com>
2019-03-08 17:20:06 +0900

    ㄴㅁㅇㅁㅇ

--- vision/src/com/display/B.java
+++ vision/src/com/display/B.java
@@ -3,7 +3,7 @@ package com.display;
 public class B {
 
 	public static void main(String[] args) {
-		String B = "황희상";
+		String B = "피카츄";
 		System.out.println(B);
 
 
--- vision/src/com/display/C.java
+++ vision/src/com/display/C.java
@@ -3,7 +3,7 @@ package com.display;
 public class C {
 
 	public static void main(String[] args) {
-		String C = "황희상";
+		String C = "파이리";
 		System.out.println(C);
 
 
